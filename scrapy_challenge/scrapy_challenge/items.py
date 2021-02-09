073065692ba494d045d6a83dd5dc0b21929d1ffa
John Garner <jdextergarneriv@gmail.com>
2021-02-09 00:01:27 -0500

    create productInfoSpider.py and develop item container for search fields

--- scrapy_challenge/scrapy_challenge/items.py
+++ scrapy_challenge/scrapy_challenge/items.py
@@ -14,3 +14,15 @@ class ScrapyChallengeItem(scrapy.Item):
     review_date = scrapy.Field()
     review_rating = scrapy.Field()
     review_text = scrapy.Field()
+
+
+class ProductReviewItem(scrapy.Item):
+
+    product_name = scrapy.Field()
+    product_brand = scrapy.Field()
+    product_source = scrapy.Field()
+    product_list_price = scrapy.Field()
+    product_sale_price = scrapy.Field()
+    product_description = scrapy.Field()
+    product_rating = scrapy.Field()
+    product_review_count = scrapy.Field()
