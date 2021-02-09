7f593baa5330700a274cdc6eb0cc785f956583fa
John Garner <jdextergarneriv@gmail.com>
2021-02-08 19:56:01 -0500

    created start urls variable with all reviews url

--- scrapy_challenge/scrapy_challenge/spiders/goProSpider.py
+++ scrapy_challenge/scrapy_challenge/spiders/goProSpider.py
@@ -5,3 +5,7 @@ from scrapy_challenge.items import ScrapyChallengeItem
 class goProSpider(scrapy.Spider):
     name = "GoProReviews"
     allowed_domains = ["amazon.com"]
+
+    start_urls = [
+        "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+    ]
