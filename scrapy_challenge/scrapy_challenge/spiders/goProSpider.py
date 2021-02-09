50a9b3265704fa7e7503d24215e3af7ee34be5b6
John Garner <jdextergarneriv@gmail.com>
2021-02-08 19:59:03 -0500

    create parse method passing self and response as arguments

--- scrapy_challenge/scrapy_challenge/spiders/goProSpider.py
+++ scrapy_challenge/scrapy_challenge/spiders/goProSpider.py
@@ -9,3 +9,6 @@ class goProSpider(scrapy.Spider):
     start_urls = [
         "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
     ]
+
+    def parse(self, response):
+        
