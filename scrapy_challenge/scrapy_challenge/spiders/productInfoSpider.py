77207946792a3f38dbe8fafedda53057eb7e596d
John Garner <jdextergarneriv@gmail.com>
2021-02-09 00:12:06 -0500

    create productInfoSpider class with name, allowed_domains and start_urls

--- scrapy_challenge/scrapy_challenge/spiders/productInfoSpider.py
+++ scrapy_challenge/scrapy_challenge/spiders/productInfoSpider.py
@@ -1,2 +1,11 @@
 import scrapy
 from scrapy_challenge.items import ProductInfoItem
+
+
+class productInfoSpider(scrapy.Spider):
+    name = "productinfo"
+    allowed_domains = ["amazon.com"]
+
+    start_urls = [
+        "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/dp/B0792MJLNM/ref=sr_1_3?crid=D3C7EDM435E7&keywords=gopro+fusion&qid=1550442454&s=electronics&sprefix=GoPro+Fu%2Celectronics%2C1332&sr=1-3"
+    ]
