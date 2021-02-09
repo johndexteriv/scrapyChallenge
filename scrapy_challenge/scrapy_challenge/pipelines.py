4a90b35e0277d05b60e4a87e657b7ef3026c18cb
John Garner <jdextergarneriv@gmail.com>
2021-02-08 23:41:26 -0500

    create connection variable in init function

--- scrapy_challenge/scrapy_challenge/pipelines.py
+++ scrapy_challenge/scrapy_challenge/pipelines.py
@@ -10,8 +10,8 @@ import pymongo
 
 
 class ScrapyChallengePipeline:
-
     def __init__(self):
+        self.conn = pymongo.MongoClient("localhost", 27017)
 
     def process_item(self, item, spider):
         return item
